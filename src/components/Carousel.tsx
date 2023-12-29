f7602f5a18c0bf67a72fd9a99af944e1d99c7e21
Jesse <jtrujillo707@gmail.comgit config --global user.name jesse>
2023-12-28 16:38:08 -0800

    changed div className

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,9 +6,9 @@ import { Carousel } from 'flowbite-react';
 // react jsx login component
 const Carousel1 = () => {
     return (
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 object-contain h-48 w-96">
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 ">
             <Carousel slideInterval={5000} >
-                <Image  fetchPriority="high" loading="eager" priority={true} width={300} height={100} className=" " src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/20231030_LiquidGold_TruckGlamorShot_PatrickChristian_V2-_2_-1-1-e1703788231665.webp" alt="truck image" />
+                <Image  fetchPriority="high" loading="eager" priority={true} width={300} height={100} className="object-contain h-48 w-96 " src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/20231030_LiquidGold_TruckGlamorShot_PatrickChristian_V2-_2_-1-1-e1703788231665.webp" alt="truck image" />
                 <Image loading="eager" width={250} height={100}   src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/FF751844-62E9-4AB1-8BDC-92CF438CE6C8-1.webp" alt="orange juice" />
                 <Image loading="eager" width={250} height={100}   src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/Untitled-2.webp" alt="purple drink" />
 
