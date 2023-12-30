bc5c183f6500edfd86d8879a972920ec25d66a7e
Jesse <jtrujillo707@gmail.comgit config --global user.name jesse>
2023-12-30 01:28:31 -0800

    carousel image size

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,9 +9,9 @@ const Carousel1 = () => {
         <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 ">
            
             <Carousel slideInterval={4000} >
-                <Image  fetchPriority="high" loading="eager" priority={true} width={300} height={100} className="object-contain h-48 w-96 " src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/20231030_LiquidGold_TruckGlamorShot_PatrickChristian_V2-_2_-1-1-e1703788231665.webp" alt="truck image" />
-                <Image loading="eager" width={250} height={100}  className="object-contain h-48 w-96" src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/FF751844-62E9-4AB1-8BDC-92CF438CE6C8-1.webp" alt="orange juice" />
-                <Image loading="eager" width={250} height={100}   className="object-contain h-48 w-96" src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/Untitled-2.webp" alt="purple drink" />
+                <Image  fetchPriority="high" loading="eager" priority={true} width={350} height={100} className="object-contain h-56 w-96 " src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/20231030_LiquidGold_TruckGlamorShot_PatrickChristian_V2-_2_-1-1-e1703788231665.webp" alt="truck image" />
+                <Image loading="eager" width={350} height={100}  className="object-contain h-56 w-96" src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/FF751844-62E9-4AB1-8BDC-92CF438CE6C8-1.webp" alt="orange juice" />
+                <Image loading="eager" width={350} height={100}   className="object-contain h-56 w-96" src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/Untitled-2.webp" alt="purple drink" />
 
             </Carousel>
         </div>
