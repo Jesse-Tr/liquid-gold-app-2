368b96cfc8e27d7a142c01eacf712327efe6413e
Jesse <jtrujillo707@gmail.comgit config --global user.name jesse>
2024-01-04 22:32:45 -0800

    touch-slide disable

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -8,7 +8,7 @@ const Carousel1 = () => {
     return (
         <div className="h-96 sm:h-96 xl:h-96 2xl:h-96 ">
            
-            <Carousel slideInterval={4000} data-bs-touch="false">
+            <Carousel slideInterval={4000} touch-none>
                 <Image  fetchPriority="high" loading="eager" priority={true} width={350} height={100} className="object-contain 2xl:h-96 h-screen w-full " src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/20231030_LiquidGold_TruckGlamorShot_PatrickChristian_V2-_2_-1-1-e1703788231665.webp" alt="truck image" />
                 <Image loading="eager" width={450} height={200}  className="object-contain 2xl:h-96 h-96 w-full" src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/14c.webp" alt="orange juice" />
                 <Image loading="eager" width={450} height={200}   className="object-contain 2xl:h-96 h-96 w-full" src="https://dev-cs55-13-2023.pantheonsite.io/wp-content/uploads/2023/12/Untitled-2.webp" alt="purple drink" />
